4acc2e8d0b9270dd9ca6edd0f1900e746924b8f2
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-19 11:27:40 +0530

    Create UserController.java

---
+++ src/main/java/com/my/sample/REST/api/UserController.java
@@ -0,0 +1,57 @@
+package com.my.sample.REST.api;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.my.sample.data.UserData;
+import com.my.sample.data.ChangePasswordData;
+import com.my.sample.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		super();
+
+		if (userService == null) {
+			throw new IllegalArgumentException("userService cannot be null");
+		}
+
+		this.userService = userService;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> getUser() throws Exception {
+		return new ResponseEntity<>(userService.getUser(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ResponseEntity<?> createUser(@RequestBody UserData userData) throws Exception {
+		return new ResponseEntity<>(userService.createUser(userData), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public ResponseEntity<?> updateUser(@RequestBody UserData userData)
+			throws Exception {
+		return new ResponseEntity<>(userService.updateUser(userData), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/changePassword", method = RequestMethod.POST)
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordData changePasswordData)
+            throws Exception {
+        return new ResponseEntity<>(userService.changePassword(changePasswordData), HttpStatus.OK);
+    }
+
+}
