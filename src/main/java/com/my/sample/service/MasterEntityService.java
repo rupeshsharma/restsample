a7171ef34ef796c7981a9f95199e6a80af58e28b
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-19 13:10:33 +0530

    Update MasterEntityService.java

--- src/main/java/com/my/sample/service/MasterEntityService.java
+++ src/main/java/com/my/sample/service/MasterEntityService.java
@@ -5,5 +5,8 @@ import com.my.sample.data.MasterEntityData;
 public interface MasterEntityService {
 
 	MasterEntityData getMasterEntity();
-
+	void addMasterEntity(String type, String value);
+	void updateMasterEntity(String type, String previousValue, String newValue);
+	void removeMasterEntity(String type, String value);
+	
 }
