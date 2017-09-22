b9ae3a589b66a9c4beaf7b9c3985381151c8d373
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-22 11:06:58 +0530

    Update User.java

--- src/main/java/com/my/sample/domain/User.java
+++ src/main/java/com/my/sample/domain/User.java
@@ -56,6 +56,14 @@ public class User implements Serializable {
 
 	@Column(name = "ROLE")
 	private String role;
+	
+	public User(Long id) {
+	    this.id = id;
+	}
+	
+	public User() {
+        
+    }
 
 	public Long getId() {
 		return id;
@@ -136,5 +144,30 @@ public class User implements Serializable {
 	public void setRole(String role) {
 		this.role = role;
 	}
+	
+	@Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 
 }
