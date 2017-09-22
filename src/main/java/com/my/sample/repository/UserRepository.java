80057f54cba2d78bb23bd0ee1335bd1160fdf307
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-22 11:10:56 +0530

    Update UserRepository.java

--- src/main/java/com/my/sample/repository/UserRepository.java
+++ src/main/java/com/my/sample/repository/UserRepository.java
@@ -1,14 +1,19 @@
 package com.my.sample.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.my.sample.data.UserData;
 import com.my.sample.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query("SELECT u from User u where u.userName = :username AND u.password = :password AND u.status = 'y'")
 	User authenticate(@Param("username") String username, @Param("password") String password);
+	
+	@Query("SELECT New UserData(u.id, u.userName, u.name, u.contact, u.role) from User u where u.status = 'y'")
+	List<UserData> getALL();
 
 }
