bb6a5209390ae11842910f63cb5398f6c7988aed
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-26 14:23:56 +0530

    Create SearchCustomerData.java

---
+++ src/main/java/com/my/sample/data/SearchCustomerData.java
@@ -0,0 +1,35 @@
+package com.my.sample.data;
+
+import java.io.Serializable;
+
+public class SearchCustomerData implements Serializable {
+
+    String name;
+    String mobile;
+    String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+}
