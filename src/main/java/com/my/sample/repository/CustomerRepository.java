3b9e452129b1b9d110ab80e89b51dadf849b9414
rupeshsharma <rupesh.sharma@globallogic.com>
2017-09-26 14:27:51 +0530

    Update CustomerRepository.java

--- src/main/java/com/my/sample/repository/CustomerRepository.java
+++ src/main/java/com/my/sample/repository/CustomerRepository.java
@@ -16,5 +16,8 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 	@Query("Select c FROM Customer c WHERE c.lastVisited <= :lastVisitedDate")
 	List<Customer> getCustomerVisitedBeforeDate(@Param("lastVisitedDate") Date lastVisitedDate);
+	
+	@Query("Select c FROM Customer c WHERE c.name LIKE CONCAT('%',:name,'%') AND c.mobile LIKE CONCAT('%',:mobile,'%') AND c.email LIKE CONCAT('%',:email,'%')")
+	List<Customer> advanceSearch(@Param("name") String name, @Param("mobile") String mobile, @Param("email") String email);
 
 }
